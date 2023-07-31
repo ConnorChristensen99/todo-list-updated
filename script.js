6ffb847b7bca990cb7835a355f907d27bf177e3a
Connor Christensen <connor.christensen@uvu.edu>
2023-07-31 12:53:36 -0600

    CSS and HTML done - need to hook up JS

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let todos = [
+    
+]
+
 let addTodoBar = document.getElementById('input')
 let clrBtn = document.getElementById('clear-btn')
+let todoList = document.getElementById('task-box')
 
-clrBtn.addEventListener('click', () => {
-    console.log(addTodoBar.value)
+addTodoBar.addEventListener('keyup', e => {
+    let userTask = addTodoBar.value;
+    if(e.key == "Enter" && userTask) {
+        console.log(userTask)
+    }
 })
